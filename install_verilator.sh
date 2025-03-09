5fb3ed8701a80fb751df630e4c821183c9a899c7
André Costa <aj.costa@campus.fct.unl.pt>
2025-03-09 14:10:23 +0000

    updated verilator install script

--- install_verilator.sh
+++ install_verilator.sh
@@ -4,14 +4,17 @@
 mkdir -p verilator
 cd verilator
 
+#install dependencies (https://github.com/verilator/verilator/blob/master/ci/docker/buildenv/Dockerfile)
+apt-get install --no-install-recommends -y autoconf bc bison build-essential ca-certificates ccache clang cmake flex gdb git gtkwave help2man libfl2 libfl-dev libgoogle-perftools-dev libsystemc libsystemc-dev numactl perl python3 wget z3 zlib1g zlib1g-dev
+
 # Clone the Verilator repository
 git clone https://github.com/verilator/verilator.git
 
 # Enter the Verilator directory
 cd verilator
 
-# Checkout the latest stable release
-git checkout stable
+# Checkout the stable release (commit ac3f30e)
+git checkout ac3f30ed6c9f1c9530bb68192396c117c14419d0
 
 # Build and install Verilator
 autoconf
